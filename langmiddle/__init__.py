7406fcabbe9aa839deb0e37f91a99477346b9a12
alpha-xone <alpha.xone@outlook.com>
2025-11-21 21:41:02 +0800

    chore(pkg): export new auth API and storage classes in package __init__

--- langmiddle/__init__.py
+++ langmiddle/__init__.py
@@ -9,6 +9,30 @@ __author__ = "Alpha x1"
 __email__ = "alpha.xone@outlook.com"
 __x__ = "alpha_xone_"
 
+from .auth import (
+    ensure_authenticated,
+    get_credentials_path,
+    get_current_user,
+    is_authenticated,
+    login_user,
+    logout_user,
+    refresh_token,
+    register_user,
+)
 from .config import StorageConfig
+from .context import ContextEngineer
+from .storage import ChatStorage
 
-__all__ = ["StorageConfig"]
+__all__ = [
+    "StorageConfig",
+    "ChatStorage",
+    "ContextEngineer",
+    "register_user",
+    "login_user",
+    "logout_user",
+    "get_current_user",
+    "is_authenticated",
+    "ensure_authenticated",
+    "refresh_token",
+    "get_credentials_path",
+]
